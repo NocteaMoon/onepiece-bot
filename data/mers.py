063da0a6cd5776d855685a54013a60c89ca0b9a4
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-08 15:17:39 +0200

    Create mers.py

---
+++ data/mers.py
@@ -0,0 +1,9 @@
+# (nom, niveau_requis, cout_berrys, cout_endurance, ile_arrivee)
+MERS = [
+    ("East Blue", 1, 0, 15, "Île de départ"),
+    ("South Blue", 15, 40, 20, "Îlot du Sud"),
+    ("West Blue", 15, 40, 20, "Rivage de l'Ouest"),
+    ("North Blue", 15, 40, 20, "Côte du Nord"),
+    ("Grand Line", 35, 120, 30, "Porte de la Ligne"),
+    ("Nouveau Monde", 70, 400, 40, "Premier Rivage du Nouveau Monde"),
+]
