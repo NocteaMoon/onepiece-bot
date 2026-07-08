ecd2dc63fcb4aabe96921ed0ebbc576220cce6d1
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-08 22:28:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,6 +24,7 @@ from cogs.navigation import setup_navigation_commands
 from cogs.peche_au_gros import setup_peche_au_gros_commands
 from cogs.combat import setup_combat_commands
 from cogs.duel import setup_duel_commands
+from cogs.casino import setup_casino_commands
 
 app = Flask('')
 
@@ -60,6 +61,7 @@ setup_navigation_commands(bot)
 setup_peche_au_gros_commands(bot)
 setup_combat_commands(bot)
 setup_duel_commands(bot)
+setup_casino_commands(bot)
 
 @bot.event
 async def on_ready():
