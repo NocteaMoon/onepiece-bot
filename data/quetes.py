2a8f52220f2c2d20cf65c95004287368374b5c7b
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-09 18:22:40 +0200

    Create quetes.py

---
+++ data/quetes.py
@@ -0,0 +1,32 @@
+OBJECTIFS = {
+    "explorer": {
+        "label": "Explorer {cible} fois",
+        "daily_min": 3, "daily_max": 6,
+        "weekly_min": 15, "weekly_max": 25,
+        "berrys_per": 15, "xp_per": 8,
+    },
+    "combattre_victoire": {
+        "label": "Remporter {cible} combat(s)",
+        "daily_min": 2, "daily_max": 4,
+        "weekly_min": 8, "weekly_max": 15,
+        "berrys_per": 25, "xp_per": 15,
+    },
+    "collecte": {
+        "label": "Pêcher, chasser ou récolter {cible} fois",
+        "daily_min": 3, "daily_max": 6,
+        "weekly_min": 15, "weekly_max": 25,
+        "berrys_per": 12, "xp_per": 6,
+    },
+    "travailler": {
+        "label": "Travailler {cible} fois",
+        "daily_min": 2, "daily_max": 3,
+        "weekly_min": 8, "weekly_max": 12,
+        "berrys_per": 20, "xp_per": 5,
+    },
+    "cuisiner": {
+        "label": "Cuisiner {cible} plat(s)",
+        "daily_min": 1, "daily_max": 2,
+        "weekly_min": 5, "weekly_max": 8,
+        "berrys_per": 30, "xp_per": 10,
+    },
+}
