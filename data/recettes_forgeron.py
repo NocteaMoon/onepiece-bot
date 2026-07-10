cef90889af99433a28b2e11bdee3208863bf730b
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-11 00:00:20 +0200

    Create recettes_forgeron.py

---
+++ data/recettes_forgeron.py
@@ -0,0 +1,6 @@
+# (nom_recette, [(nom_ingredient, quantite), ...], nom_objet_resultat, rang_requis)
+RECETTES_FORGERON = [
+    ("Dague artisanale", [("Minerai de fer", 2)], "Dague artisanale", 0),
+    ("Sabre forgé maison", [("Minerai de fer", 3), ("Minerai d'étain", 1)], "Sabre forgé maison", 1),
+    ("Armure d'acier bleu", [("Pépite d'acier bleu", 2), ("Minerai d'étain", 2)], "Armure d'acier bleu", 2),
+]
