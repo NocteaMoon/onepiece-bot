52c6bf13e759b5a562fed25855d10ac76964ecd5
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-11 00:22:19 +0200

    Create recettes_charpentier.py

---
+++ data/recettes_charpentier.py
@@ -0,0 +1,5 @@
+RECETTES_CHARPENTIER = [
+    ("Radeau renforcé", [("Planche de bois flotté", 3)], "Radeau renforcé", 0),
+    ("Voilier artisanal", [("Planche de bois flotté", 4), ("Corde tressée", 2)], "Voilier artisanal", 1),
+    ("Navire du charpentier légendaire", [("Voile renforcée", 2), ("Corde tressée", 3)], "Navire du charpentier légendaire", 2),
+]
