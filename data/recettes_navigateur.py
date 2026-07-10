dd9c481f4937a637c6a1e7f27e719f1d22b91c18
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-11 00:01:38 +0200

    Create recettes_navigateur.py

---
+++ data/recettes_navigateur.py
@@ -0,0 +1,5 @@
+RECETTES_NAVIGATEUR = [
+    ("Carte des courants côtiers", [("Parchemin usé", 2)], "Carte des courants côtiers", 0),
+    ("Atlas des vents", [("Encre de seiche", 2), ("Parchemin usé", 1)], "Atlas des vents", 1),
+    ("Carte des abysses lumineuses", [("Corail luminescent", 2), ("Encre de seiche", 1)], "Carte des abysses lumineuses", 2),
+]
