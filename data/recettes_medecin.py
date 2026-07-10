c3df5e94c59c1ce509b938bbc9d6d7d38de638ac
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-11 00:01:03 +0200

    Create recettes_medecin.py

---
+++ data/recettes_medecin.py
@@ -0,0 +1,5 @@
+RECETTES_MEDECIN = [
+    ("Tisane apaisante", [("Herbe apaisante", 2)], "Tisane apaisante", 0),
+    ("Baume de saule", [("Feuille de saule marin", 2)], "Baume de saule", 1),
+    ("Élixir de vitalité", [("Racine de vitalité", 2), ("Herbe apaisante", 1)], "Élixir de vitalité", 2),
+]
