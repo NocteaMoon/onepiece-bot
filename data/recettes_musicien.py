cada980e2d25cfe1bfe0c8d5c9a3eb5ebc90b1c0
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-11 11:15:35 +0200

    Create recettes_musicien.py

---
+++ data/recettes_musicien.py
@@ -0,0 +1,5 @@
+RECETTES_MUSICIEN = [
+    ("Partition improvisée", [("Corde de luth", 2)], "Partition improvisée", 0),
+    ("Partition entraînante", [("Corde de luth", 3), ("Roseau à vent", 1)], "Partition entraînante", 1),
+    ("Symphonie de la mer", [("Parchemin de mélodie", 2), ("Roseau à vent", 1)], "Symphonie de la mer", 2),
+]
