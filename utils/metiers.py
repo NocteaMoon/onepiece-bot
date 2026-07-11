924257b89f0968e911221a764e5d05642941f9a6
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-11 11:15:59 +0200

    Update metiers.py

--- utils/metiers.py
+++ utils/metiers.py
@@ -1,4 +1,4 @@
-METIERS_DISPONIBLES = ["Cuisinier", "Forgeron", "Médecin", "Navigateur", "Charpentier"]
+METIERS_DISPONIBLES = ["Cuisinier", "Forgeron", "Médecin", "Navigateur", "Charpentier", "Musicien"]
 
 RANG_LABELS = {0: "Apprenti", 1: "Confirmé", 2: "Maître"}
 SEUILS_RANG = {0: 0, 1: 150, 2: 400}
