23594c87ec9e718229296bc7f172197859614546
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-11 11:34:05 +0200

    Create recettes_archeologue.py

---
+++ data/recettes_archeologue.py
@@ -0,0 +1,5 @@
+RECETTES_ARCHEOLOGUE = [
+    ("Parchemin déchiffré", [("Fragment de poterie ancienne", 2)], "Parchemin déchiffré", 0),
+    ("Amulette ancienne restaurée", [("Fragment de poterie ancienne", 3), ("Éclat de pierre gravée", 1)], "Amulette ancienne restaurée", 1),
+    ("Trésor des temps perdus", [("Médaillon oublié", 2), ("Éclat de pierre gravée", 1)], "Trésor des temps perdus", 2),
+]
