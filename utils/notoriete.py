bd0516ab03cb8ec6581244dacaa572a53a00b0d5
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-12 22:24:12 +0200

    Create notoriete.py

---
+++ utils/notoriete.py
@@ -0,0 +1,14 @@
+from database.db import get_pool
+
+MONTANT_BOSS_MONDIAL = 3
+MONTANT_TOURNOI = 8
+MONTANT_SUCCES = 2
+
+
+async def add_notoriete(guild_id: int, user_id: int, montant: int):
+    pool = get_pool()
+    async with pool.acquire() as conn:
+        await conn.execute(
+            "UPDATE players SET notoriete = notoriete + $3 WHERE guild_id=$1 AND user_id=$2",
+            guild_id, user_id, montant
+        )
