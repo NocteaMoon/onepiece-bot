173bfb37ee8e67e809d7870e81e41ac251154d0a
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-15 23:00:32 +0200

    Create buffs_cuisine.py

---
+++ data/buffs_cuisine.py
@@ -0,0 +1,7 @@
+# nom_du_plat -> (stat, valeur, duree_minutes)
+BUFFS_CUISINE = {
+    "Ragoût de sanglier": ("force", 3, 15),
+    "Poisson tigre grillé": ("vitesse", 3, 15),
+    "Tarte de la moisson": ("agilite", 3, 15),
+    "Festin du capitaine": ("force", 6, 30),
+}
