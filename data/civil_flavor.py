30d86237b15980c5aa649f86c81c4ceeb9c8c88b
NocteaMoon <bv8p9t8f7t@privaterelay.appleid.com>
2026-07-21 23:42:41 +0200

    Create civil_flavor.py

---
+++ data/civil_flavor.py
@@ -0,0 +1,26 @@
+# Contenu varié pour les mini-jeux Civils
+
+OBJETS_ENCHERES = [
+    "un vieux coffret aux gravures effacées",
+    "une statuette rapportée d'une île lointaine",
+    "un rouleau de tissu d'une qualité étrange",
+    "une horloge dont le mécanisme semble unique",
+    "un lot de porcelaine récupéré d'une épave",
+    "un miroir ancien au cadre travaillé",
+    "une collection de pièces d'un royaume disparu",
+]
+
+NPCS_NEGOCIATION = [
+    "un marchand ambulant pressé de conclure",
+    "une commerçante coriace mais juste",
+    "un vieux négociant qui a tout vu",
+    "un jeune vendeur encore inexpérimenté",
+    "une tenancière de comptoir réputée intraitable",
+    "un colporteur qui adore marchander",
+]
+
+RARETE_RECOMPENSE_ARTISANAT = {
+    "Commun": (20, 40, 0.05),
+    "Aiguisé": (40, 70, 0.08),
+    "Grade": (70, 120, 0.12),
+}
